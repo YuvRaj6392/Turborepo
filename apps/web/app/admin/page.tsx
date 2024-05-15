b873ae78c4aeb3d377bc2194f83446b63d572c7b
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-05-15 22:40:57 +0530

    added

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Admin from "@repo/ui/admin"
+import {InputBox } from "@repo/ui/input-box"
+export default function page() {
+  return (
+    <div>
+      <Admin/>
+      <InputBox/>
+    </div>
+  )
+}
